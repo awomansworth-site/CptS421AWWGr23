cb471ce86a518b2cd51e97bbf15f9e20ec3205e4
Kyle-Jangwsu <122514272+Kyle-Jangwsu@users.noreply.github.com>
2026-04-22 02:13:55 -0700

    Add files via upload

--- code/apps/cms/types/generated/components.d.ts
+++ code/apps/cms/types/generated/components.d.ts
@@ -1,3 +1,17 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface AProductListingBlock extends Struct.ComponentSchema {
+  collectionName: 'components_a_product_listing_blocks';
+  info: {
+    displayName: 'Product listing block';
+  };
+  attributes: {};
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'a.product-listing-block': AProductListingBlock;
+    }
+  }
+}
